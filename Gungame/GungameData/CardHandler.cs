12fdce0332dd87f27a2982cadf0b27e22c6c8a7d
viktorferenczi <ferencziviktor11@gmail.com>
2020-02-18 12:18:39 +0100

    adding

--- Gungame/GungameData/CardHandler.cs
+++ Gungame/GungameData/CardHandler.cs
@@ -6,12 +6,33 @@ namespace Gungame.GungameData
 {
     class CardHandler
     {
-        public List<Card> deck = new List<Card>() ;
-       
+        public List<Card> deck = new List<Card>();
+        
 
-        public List<Card> Dealer(Player player)
+
+        protected int GenerateRandomNumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, 33);
+
+        }
+
+        public void Dealer(Player player1, Player player2)
         {
 
+            for (int i = 0; i < 5; i++)
+            {
+                Card card = deck[GenerateRandomNumber()];
+                player1.hand.Add(card);
+                deck.Remove(card);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Card card = deck[GenerateRandomNumber()];
+                player2.hand.Add(card);
+                deck.Remove(card);
+            }
         }
         
        
